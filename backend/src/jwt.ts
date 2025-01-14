06aae1eff3799ce1ef9161398e5d62259c977ab7
Axel Leblanc <59664317+axllblc@users.noreply.github.com>
2025-01-15 00:54:42 +0100

    Backend: OAuth, JWT...

---
+++ backend/src/jwt.ts
@@ -0,0 +1,17 @@
+import * as jose from "jose";
+
+const JWT_ALGORITHM = "HS256";
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function signJWT(payload: any) {
+    return await new jose.SignJWT(payload)
+        .setProtectedHeader({ alg: JWT_ALGORITHM })
+        .setIssuedAt()
+        .setExpirationTime("1d")
+        .sign(JWT_SECRET);
+}
+
+export async function decodeJWT(jwt: string) {
+    const {payload, protectedHeader} = await jose.jwtVerify(jwt, JWT_SECRET);
+    return {payload, protectedHeader};
+}
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,29 +1,84 @@
 import {Router} from "express";
 import {query, validationResult} from "express-validator";
+import axios from "axios";
+import {signJWT} from "../jwt.js";
+
+const DISCORD_AUTHORIZATION_URL = "https://discord.com/oauth2/authorize";
+const DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token";
+const DISCORD_USER_DATA_URL = "https://discord.com/api/v10/users/@me";
 
 export default Router()
 
     // Start the authorization process with Discord OAuth2
     // HTTP redirect to the Discord OAuth2 authorization server
     .get("/", (req, res) => {
-        // TODO redirect
-        res.status(500).send("Not implemented");
+        const authorizationUrl = new URL(DISCORD_AUTHORIZATION_URL);
+        authorizationUrl.searchParams.set("client_id", process.env.DISCORD_CLIENT_ID!);
+        authorizationUrl.searchParams.set("response_type", "code");
+        authorizationUrl.searchParams.set("redirect_uri", process.env.DISCORD_REDIRECT_URI!);
+        authorizationUrl.searchParams.set("scope", "identify");
+
+        res.redirect(authorizationUrl.toString());
     })
 
     // Discord's authorization server redirects the user here. The query string contains the authorization code.
     .get("/callback",
         query("code").isString().notEmpty(),
-        (req, res) => {
+        async (req, res) => {
+            const redirectUrl = new URL(process.env.FRONTEND_REDIRECT_URI!);
             const validation = validationResult(req);
 
             if (validation.isEmpty()) {
-                // TODO request a token using the code
-                // TODO get Discord ID
-                // TODO other things... (JWT, create session ID...)
-                // TODO redirect to the front-end
-                res.status(500).send("Not implemented");
+                try {
+                    // Request a token using the code
+                    const tokenResponse = await axios.post(
+                        DISCORD_TOKEN_URL,
+                        new URLSearchParams({
+                            client_id: process.env.DISCORD_CLIENT_ID!,
+                            client_secret: process.env.DISCORD_CLIENT_SECRET!,
+                            grant_type: "authorization_code",
+                            code: req.query!.code,
+                            redirect_uri: process.env.DISCORD_REDIRECT_URI!,
+                        }),
+                        {
+                            headers: {
+                                "Content-Type": "application/x-www-form-urlencoded",
+                            },
+                        }
+                    );
+
+                    // Get Discord ID
+                    const userDataResponse = await axios.get(
+                        DISCORD_USER_DATA_URL,
+                        {
+                            headers: {
+                                "Authorization": `${tokenResponse.data.token_type} ${tokenResponse.data.access_token}`,
+                            }
+                        }
+                    );
+
+                    // JSON Web Token
+                    const jwt = await signJWT({
+                        id: userDataResponse.data.id,
+                        username: userDataResponse.data.username
+                    });
+
+                    res.cookie("access_token", jwt, {
+                        httpOnly: true,
+                        maxAge: 24 * 60 * 60 * 1000,
+                    });
+
+                    // TODO other things... (create session ID...)
+                } catch(reason) {
+                    console.error(reason);
+                    res.status(500).send("Internal Server Error");
+                    return;
+                }
             } else {
-                res.status(400).send({error: validation.array()})
+                redirectUrl.searchParams.set("error", req.query?.error ?? "unknown");
+                redirectUrl.searchParams.set("error_description", req.query?.error_description ?? "unknown");
             }
+
+            res.redirect(redirectUrl.toString());
         }
     );
