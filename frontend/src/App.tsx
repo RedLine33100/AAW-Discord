8f136e760c306ad46494f1a6967784c24ebac726
redline <account@ngimenez.fr>
2025-01-18 05:41:59 +0100

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
         return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
     };
     const logout = () => {
-        localStorage.removeItem("access_token");
+        localStorage.clear();
         setIsAuthenticated(false)
     }
 
